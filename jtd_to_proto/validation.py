e992fa4e022094f8aef2f0a4b4be2486cac5f66a
Gabe Goodhart <ghart@us.ibm.com>
2023-03-15 15:48:41 -0600

    InternalValidation: Full implementation of jtd validation with tests
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- jtd_to_proto/validation.py
+++ jtd_to_proto/validation.py
@@ -4,27 +4,31 @@ This module implements recursive JTD schema validation
 https://www.rfc-editor.org/rfc/rfc8927
 """
 # Standard
-from typing import Any, Dict, List, Optional
+from datetime import datetime
+from typing import Any, Callable, Dict, List, Optional
 
 # First Party
 import alog
 
 log = alog.use_channel("JTD2P")
 
+# Map of type validators for standard JTD types
+JTD_TYPE_VALIDATORS = {
+    "boolean": lambda x: isinstance(x, bool),
+    "float32": lambda x: isinstance(x, (int, float)),
+    "float64": lambda x: isinstance(x, (int, float)),
+    "int8": lambda x: isinstance(x, int),
+    "uint8": lambda x: isinstance(x, int) and x >= 0,
+    "int16": lambda x: isinstance(x, int),
+    "uint16": lambda x: isinstance(x, int) and x >= 0,
+    "int32": lambda x: isinstance(x, int),
+    "uint32": lambda x: isinstance(x, int) and x >= 0,
+    "string": lambda x: isinstance(x, str),
+    "timestamp": lambda x: isinstance(x, datetime),
+}
+
 # List of standard type names
-JTD_TYPES = [
-    "boolean",
-    "float32",
-    "float64",
-    "int8",
-    "uint8",
-    "int16",
-    "uint16",
-    "int32",
-    "uint32",
-    "string",
-    "timestamp",
-]
+JTD_TYPES = list(JTD_TYPE_VALIDATORS.keys())
 
 
 def is_valid_jtd(
@@ -47,9 +51,38 @@ def is_valid_jtd(
     return _is_valid_jtd_impl(schema, valid_types, is_root_schema=True)
 
 
+def validate_jtd(
+    obj: Any,
+    schema: Dict[str, Any],
+    type_validators: Optional[Dict[str, Callable[[Any], bool]]] = None,
+) -> bool:
+    """Validate the given object against the given schema
+
+    Args:
+        obj (Any)
+            The candidate object to validate
+        schema (Dict[str, Any])
+            The schema to validate against
+        type_validators (Optional[Dict[str, Callable[[Any], bool]]])
+            Mapping from types string names to validation functions
+
+    Returns:
+        is_valid (bool)
+            True if the object matches the schema, False otherwise
+    """
+    type_validators = type_validators or JTD_TYPE_VALIDATORS
+    if not is_valid_jtd(schema, type_validators.keys()):
+        raise ValueError(f"Invalid schema: {schema}")
+    return _validate_jtd_impl(obj, schema, type_validators, is_root_schema=True)
+
+
 ## Implementation ##############################################################
 
 _SHARED_KEYS = {"nullable", "metadata", "definitions"}
+
+
+def _is_string_key_dict(value: Any) -> bool:
+    return isinstance(value, dict) and all(isinstance(key, str) for key in value)
 
 
 def _is_valid_jtd_impl(
@@ -195,6 +228,11 @@ def _is_valid_jtd_impl(
                 not _is_valid_jtd_impl(val, valid_types, definitions)
                 for val in mapping_val.values()
             )
+            # Mapping entries are of the "properties" form
+            or any(
+                "properties" not in val and "optionalProperties" not in val
+                for val in mapping_val.values()
+            )
             # Mapping entry "nullable" matches discriminator "nullable"
             or any(
                 val.get("nullable", False) != nullable for val in mapping_val.values()
@@ -225,5 +263,148 @@ def _is_valid_jtd_impl(
     return False
 
 
-def _is_string_key_dict(value: Any) -> bool:
-    return isinstance(value, dict) and all(isinstance(key, str) for key in value)
+def _validate_jtd_impl(
+    obj: Any,
+    schema: Dict[str, Any],
+    type_validators: Dict[str, Callable[[Any], bool]],
+    definitions: Optional[Dict[str, Any]] = None,
+    *,
+    is_root_schema: bool = False,
+):
+    """Recursive validation implementation"""
+
+    # Pull out common definitions from the root that will be passed along
+    # everywhere
+    definitions = definitions or {}
+    if is_root_schema:
+        definitions = schema.get("definitions", {})
+
+    # Check to see if this schema is null and nullable
+    if obj is None and schema.get("nullable", False):
+        return True
+
+    # Get the set of keys in this schema with universal keys removed
+    schema_keys = set(schema.keys()) - _SHARED_KEYS
+
+    # Empty (3.3.1)
+    if not schema_keys:
+        return True
+
+    # Ref (3.3.2)
+    if schema_keys == {"ref"}:
+        ref_val = schema["ref"]
+        if not _validate_jtd_impl(
+            obj, definitions[ref_val], type_validators, definitions
+        ):
+            log.debug4("Invalid value <%s> or ref <%s>", obj, ref_val)
+            return False
+        return True
+
+    # Type (3.3.3)
+    if schema_keys == {"type"}:
+        type_val = schema["type"]
+        validator = type_validators.get(type_val)
+        if not (validator is not None and validator(obj)):
+            log.debug4("Invalid value <%s> for type <%s>", obj, type_val)
+            return False
+        return True
+
+    # Enum (3.3.4)
+    if schema_keys == {"enum"}:
+        enum_vals = schema["enum"]
+        if obj not in enum_vals:
+            log.debug4("Invalid enum value <%s> for enum <%s>", obj, enum_vals)
+            return False
+        return True
+
+    # Elements (3.3.5)
+    if schema_keys == {"elements"}:
+        element_schema = schema["elements"]
+        if not isinstance(obj, list) or any(
+            not _validate_jtd_impl(entry, element_schema, type_validators, definitions)
+            for entry in obj
+        ):
+            log.debug4(
+                "Invalid elements value <%s> for element schema <%s>",
+                obj,
+                element_schema,
+            )
+            return False
+        return True
+
+    # Properties (3.3.6)
+    if "properties" in schema_keys or "optionalProperties" in schema_keys:
+        if not _is_string_key_dict(obj):
+            log.debug4("Invalid properties <%s> is not a string key dict", obj)
+            return False
+        schema_properties = schema.get("properties", {})
+        schema_opt_properties = schema.get("optionalProperties", {})
+        if any(
+            prop not in obj
+            or not _validate_jtd_impl(
+                obj[prop],
+                prop_schema,
+                type_validators,
+                definitions,
+            )
+            for prop, prop_schema in schema_properties.items()
+        ):
+            log.debug4(
+                "Invalid properties <%s> for properties %s", obj, schema_properties
+            )
+            return False
+        if any(
+            prop in obj
+            and not _validate_jtd_impl(
+                obj[prop], prop_schema, type_validators, definitions
+            )
+            for prop, prop_schema in schema_opt_properties.items()
+        ):
+            log.debug4(
+                "Invalid optional properties <%s> for optional properties %s",
+                obj,
+                schema_opt_properties,
+            )
+            return False
+        all_props = set.union(
+            set(schema_properties.keys()), schema_opt_properties.keys()
+        )
+        if (
+            not schema.get("additionalProperties", False)
+            and set(obj.keys()) - all_props - _SHARED_KEYS
+        ):
+            log.debug4("Invalid additional properties in <%s> for %s", obj, schema)
+            return False
+        return True
+
+    # Values (3.3.7)
+    if schema_keys == {"values"}:
+        value_schema = schema["values"]
+        if not _is_string_key_dict(obj) or any(
+            not _validate_jtd_impl(entry, value_schema, type_validators, definitions)
+            for entry in obj.values()
+        ):
+            log.debug4("Invalid values <%s> for values schema <%s>", obj, value_schema)
+            return False
+        return True
+
+    # Discriminator (3.3.8)
+    if schema_keys == {"discriminator", "mapping"}:
+        if not _is_string_key_dict(obj):
+            log.debug4("Invalid discriminator <%s> which is not a string key dict", obj)
+            return False
+        schema_discriminator = schema["discriminator"]
+        schema_mapping = schema["mapping"]
+        discriminator_val = obj.get(schema_discriminator)
+        if discriminator_val not in schema_mapping or not _validate_jtd_impl(
+            {key: val for key, val in obj.items() if key != schema_discriminator},
+            schema_mapping[discriminator_val],
+            type_validators,
+            definitions,
+        ):
+            log.debug4("Invalid discriminator <%s> for schema %s", obj, schema)
+            return False
+        return True
+
+    # Since the schema must be valid, we should never get here!
+    raise ValueError(f"Programming Error: unhandled schema {schema}")
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -10,9 +10,11 @@ import pytest
 import alog
 
 # Local
-from jtd_to_proto.validation import is_valid_jtd
+from jtd_to_proto.validation import _validate_jtd_impl, is_valid_jtd, validate_jtd
 
 log = alog.use_channel("TEST")
+
+## is_valid_jtd ################################################################
 
 VALID_SCHEMAS = [
     # Empty
@@ -143,6 +145,10 @@ INVALID_SCHEMAS = [
             }
         },
     },
+    {
+        "discriminator": "key",
+        "mapping": {"int": {"type": "int32"}, "str": {"type": "string"}},
+    },
 ]
 
 
@@ -158,3 +164,261 @@ def test_invalid_schemas(schema):
     """Make sure all invalid schemas return False as expected"""
     log.debug("Testing invalid schema: %s", schema)
     assert not is_valid_jtd(schema)
+
+
+## validate_jtd ################################################################
+
+
+class CustomClass:
+    pass
+
+
+# (object, schema)
+VALID_JTD = [
+    # Empty
+    ({"foo": 1234, "bar": CustomClass()}, {}),
+    (None, {"nullable": True}),
+    (CustomClass(), {"metadata": {"foo": "bar"}}),
+    # Ref
+    (123, {"definitions": {"a": {"type": "float32"}}, "ref": "a"}),
+    (None, {"definitions": {"a": {"type": "float32"}}, "ref": "a", "nullable": True}),
+    # Type
+    (123, {"type": "int32"}),
+    (123, {"type": "float64"}),
+    (1.23, {"type": "float64"}),
+    (None, {"type": "boolean", "nullable": True}),
+    # Enum
+    ("FOO", {"enum": ["FOO", "BAR"]}),
+    (None, {"enum": ["FOO", "BAR"], "nullable": True}),
+    # Elements
+    ([1, 2], {"elements": {"type": "int32"}}),
+    ([], {"elements": {"type": "int32"}}),
+    (None, {"elements": {"type": "int32"}, "nullable": True}),
+    (
+        [{"foo": 1}, {"foo": 2}],
+        {"elements": {"properties": {"foo": {"type": "int32"}}}},
+    ),
+    # Properties
+    ({"foo": 123}, {"properties": {"foo": {"type": "int32"}}}),
+    ({"foo": ["bar"]}, {"properties": {"foo": {"elements": {"type": "string"}}}}),
+    (
+        {"foo": 123, "bar": "baz"},
+        {"properties": {"foo": {"type": "int32"}, "bar": {"type": "string"}}},
+    ),
+    (
+        {"foo": 123, "bar": "baz"},
+        {
+            "properties": {"foo": {"type": "int32"}},
+            "optionalProperties": {"bar": {"type": "string"}},
+        },
+    ),
+    (
+        {"foo": 123},
+        {
+            "properties": {"foo": {"type": "int32"}},
+            "optionalProperties": {"bar": {"type": "string"}},
+        },
+    ),
+    ({}, {"optionalProperties": {"bar": {"type": "string"}}}),
+    (
+        {"buz": 123},
+        {
+            "optionalProperties": {"bar": {"type": "string"}},
+            "additionalProperties": True,
+        },
+    ),
+    # Values
+    ({"foo": 123, "bar": -2}, {"values": {"type": "int32"}}),
+    ({"foo": {"bar": -2}}, {"values": {"properties": {"bar": {"type": "int32"}}}}),
+    # Discriminator
+    (
+        {"key": "str", "val": "this is a test"},
+        {
+            "discriminator": "key",
+            "mapping": {
+                "int": {"properties": {"val": {"type": "int32"}}},
+                "str": {"properties": {"val": {"type": "string"}}},
+            },
+        },
+    ),
+    (
+        {"key": "int", "val": 123},
+        {
+            "discriminator": "key",
+            "mapping": {
+                "int": {"properties": {"val": {"type": "int32"}}},
+                "str": {"properties": {"val": {"type": "string"}}},
+            },
+        },
+    ),
+    (
+        {"key": "int", "val_int": 123},
+        {
+            "discriminator": "key",
+            "mapping": {
+                "int": {"properties": {"val_int": {"type": "int32"}}},
+                "str": {"properties": {"val_str": {"type": "string"}}},
+            },
+        },
+    ),
+    (
+        {"key": "str", "val_str": "asdf"},
+        {
+            "discriminator": "key",
+            "mapping": {
+                "int": {"properties": {"val_int": {"type": "int32"}}},
+                "str": {"properties": {"val_str": {"type": "string"}}},
+            },
+        },
+    ),
+    (
+        {"key": "str", "val": "this is a test", "something": "else"},
+        {
+            "discriminator": "key",
+            "mapping": {
+                "int": {"properties": {"val": {"type": "int32"}}},
+                "str": {
+                    "properties": {"val": {"type": "string"}},
+                    "additionalProperties": True,
+                },
+            },
+        },
+    ),
+]
+
+INVALID_JTD = [
+    # Ref
+    (None, {"definitions": {"a": {"type": "float32"}}, "ref": "a", "nullable": False}),
+    ({"foo": "bar"}, {"definitions": {"a": {"type": "float32"}}, "ref": "a"}),
+    # Type
+    (1.23, {"type": "int8"}),
+    (-2, {"type": "uint8"}),
+    (None, {"type": "boolean"}),
+    # Enum
+    ("BAZ", {"enum": ["FOO", "BAR"]}),
+    (0, {"enum": ["FOO", "BAR"]}),
+    ({}, {"enum": ["FOO", "BAR"]}),
+    (None, {"enum": ["FOO", "BAR"]}),
+    # Elements
+    ([1, 2, "foo"], {"elements": {"type": "int32"}}),
+    (None, {"elements": {"type": "int32"}, "nullable": False}),
+    (
+        [{"foo": 1}, {"foo": 2}],
+        {"elements": {"properties": {"foo": {"type": "string"}}}},
+    ),
+    # Properties
+    ({"foo": 123}, {"properties": {"foo": {"type": "string"}}}),
+    ({"foo": [123]}, {"properties": {"foo": {"elements": {"type": "string"}}}}),
+    (
+        {"bar": "baz"},
+        {"properties": {"foo": {"type": "int32"}, "bar": {"type": "string"}}},
+    ),
+    (
+        {"bar": "baz"},
+        {
+            "properties": {"foo": {"type": "int32"}},
+            "optionalProperties": {"bar": {"type": "string"}},
+        },
+    ),
+    (
+        {},
+        {
+            "properties": {"foo": {"type": "int32"}},
+            "optionalProperties": {"bar": {"type": "string"}},
+        },
+    ),
+    ({"buz": 123}, {"optionalProperties": {"bar": {"type": "string"}}}),
+    (
+        {"buz": 123},
+        {
+            "optionalProperties": {"bar": {"type": "string"}},
+            "additionalProperties": False,
+        },
+    ),
+    ({"bar": 123}, {"optionalProperties": {"bar": {"type": "string"}}}),
+    ([{"foo": 123}], {"properties": {"foo": {"type": "string"}}}),
+    # Values
+    ({"foo": 123, "bar": "asdf"}, {"values": {"type": "int32"}}),
+    ({"foo": {"bar": "test"}}, {"values": {"properties": {"bar": {"type": "int32"}}}}),
+    # Discriminator
+    (
+        {"key": "str", "val": 123},
+        {
+            "discriminator": "key",
+            "mapping": {
+                "int": {"properties": {"val": {"type": "int32"}}},
+                "str": {"properties": {"val": {"type": "string"}}},
+            },
+        },
+    ),
+    (
+        {"key": "int", "val": "asdf"},
+        {
+            "discriminator": "key",
+            "mapping": {
+                "int": {"properties": {"val": {"type": "int32"}}},
+                "str": {"properties": {"val": {"type": "string"}}},
+            },
+        },
+    ),
+    (
+        {"key": "str", "val": "this is a test", "something": "else"},
+        {
+            "discriminator": "key",
+            "mapping": {
+                "int": {"properties": {"val": {"type": "int32"}}},
+                "str": {"properties": {"val": {"type": "string"}}},
+            },
+        },
+    ),
+    (
+        123,
+        {
+            "discriminator": "key",
+            "mapping": {
+                "int": {"properties": {"val": {"type": "int32"}}},
+                "str": {"properties": {"val": {"type": "string"}}},
+            },
+        },
+    ),
+]
+
+
+@pytest.mark.parametrize("obj,schema", VALID_JTD)
+def test_valid_jtd(obj, schema):
+    """Test all valid object validations"""
+    log.debug("Comparing %s to %s", obj, schema)
+    assert validate_jtd(obj, schema)
+
+
+@pytest.mark.parametrize("obj,schema", INVALID_JTD)
+def test_invalid_jtd(obj, schema):
+    """Test all invalid object validations"""
+    log.debug("Comparing %s to %s", obj, schema)
+    assert not validate_jtd(obj, schema)
+
+
+def test_custom_type_validator():
+    """Make sure that a custom type validator works as expected"""
+    assert validate_jtd(
+        CustomClass(),
+        {"type": "CustomClass"},
+        {"CustomClass": lambda x: isinstance(x, CustomClass)},
+    )
+    assert not validate_jtd(
+        123,
+        {"type": "CustomClass"},
+        {"CustomClass": lambda x: isinstance(x, CustomClass)},
+    )
+
+
+def test_validate_jtd_invalid_schema():
+    """Make sure that an invalid schema causes an error in validate_jtd"""
+    with pytest.raises(ValueError):
+        validate_jtd({}, {"not": "a valid schema"})
+
+
+def test_validate_jtd_impl_invalid_schema():
+    """COV! Make sure an error is raised if somehow the schema isn't valid"""
+    with pytest.raises(ValueError):
+        _validate_jtd_impl({}, {"invalid": "schema"}, {})
