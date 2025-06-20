81ff286858852183398604d8608f686d230ee56b
Gabe Goodhart <ghart@us.ibm.com>
2025-06-20 15:07:36 -0600

    feat: Extend compatibility to the full creation of service classes
    Branch: update_protobuf
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- py_to_proto/compat.py
+++ py_to_proto/compat.py
@@ -2,8 +2,33 @@
 Compatibility module for API changes between different versions of protobuf
 """
 
-try:
-    # protobuf >= 6
+# Standard
+from typing import Type
+import types
+
+# Third Party
+from google.protobuf.descriptor import ServiceDescriptor
+
+
+# protobuf >= 6
+try:  # pragma: no cover
     from google.protobuf.service_reflection import GeneratedServiceType
-except ImportError:
+
+    def make_service_class(service_descriptor: ServiceDescriptor) -> Type[GeneratedServiceType]:
+        return GeneratedServiceType(
+            service_descriptor.name,
+            (),
+            {"DESCRIPTOR": service_descriptor},
+        )
+
+# protobuf < 6
+except ImportError:  # pragma: no cover
     from google.protobuf.service import Service as GeneratedServiceType
+
+    def make_service_class(service_descriptor: ServiceDescriptor) -> Type[GeneratedServiceType]:
+        return types.new_class(
+            service_descriptor.name,
+            (GeneratedServiceType,),
+            {"metaclass": GeneratedServiceType},
+            lambda ns: ns.update({"DESCRIPTOR": service_descriptor}),
+        )
